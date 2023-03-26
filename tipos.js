9673131e1dd97a52648e78bfb4c5eb8cfbcb0020
Lucas Marques <marquesfaria99@gmail.com>
2023-03-26 12:09:15 -0300

    exemplos

---
+++ tipos.js
@@ -0,0 +1,28 @@
+/*
+
+const meuNumero = 3;
+
+const num1 = 1;
+const num2 = 2;
+
+const opMatematica = num1 * num2;
+
+console.log(opMatematica)
+
+
+// ponto flutuante 
+
+const flutuante = 3.3;
+
+const flutuante2 = .5;
+
+const somaFlutuante = flutuante + flutuante2;
+
+console.log(somaFlutuante)
+
+*/
+
+const n1 = 0;
+const n2 = 50;
+
+console.log(n1 === n2);
